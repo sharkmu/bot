e03f1b7345a3ceccb3d8ee07af7b193f3078e0f8
Samu <csonka.samuel@gmail.com>
2023-09-08 22:04:16 +0200

    added some basics
    - status message
    - custom help command (removed default)

--- main.py
+++ main.py
@@ -6,10 +6,25 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
+# removes default help command
+bot.remove_command('help')
+
 TOKEN = os.getenv('TOKEN')
+
+website = "https://www.google.com/"
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name='made by: 1814sam'))
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('BOT ping: {} ms'.format(round(bot.latency * 1000)))
+
+@bot.command()
+async def help(ctx):
+    user = ctx.message.author
+    await user.send(f"Here is our website for more information: {website}")
+    await ctx.reply('I sent you a DM!', mention_author=True)
 
 bot.run(TOKEN)
